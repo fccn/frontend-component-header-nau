8260291fa89ce6bdcd698993c498a4e215c28eb8
Sandro Costa <sandro.costa@fccn.pt>
2025-05-14 12:11:34 +0000

    fix: logo container height and alignment

--- src/studio-header/HeaderBody.jsx
+++ src/studio-header/HeaderBody.jsx
@@ -72,7 +72,7 @@ const HeaderBody = ({
               </Button>
             ) : (
               <div className="w-25">
-                <Row className="m-0 flex-nowrap">
+                <Row className="m-0 flex-nowrap align-items-center">
                   {renderBrandNav}
                   <CourseLockUp
                     {...{
